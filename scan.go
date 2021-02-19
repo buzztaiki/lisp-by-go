c07f5f25e2990a0327312f297e6512a420d32143
Taiki Sugawara <buzz.taiki@gmail.com>
2021-02-20 01:04:45 +0900

    scanner

---
+++ scan.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"bufio"
+	"io"
+	"strings"
+	"unicode"
+)
+
+type scanner struct {
+	src io.RuneScanner
+}
+
+func newScanner(src io.Reader) *scanner {
+	return &scanner{bufio.NewReader(src)}
+}
+
+func (sc *scanner) scan() (string, error) {
+	c, err := sc.skipSpaces()
+	if err != nil {
+		return "", err
+	}
+
+	if c == '(' || c == ')' {
+		return string(c), nil
+	}
+
+	return sc.scanToken(c)
+}
+
+func (sc *scanner) skipSpaces() (rune, error) {
+	for {
+		c, _, err := sc.src.ReadRune()
+		if err != nil {
+			return 0, err
+		}
+		if !unicode.IsSpace(c) {
+			return c, nil
+		}
+	}
+}
+
+func (sc *scanner) scanToken(head rune) (string, error) {
+	buf := &strings.Builder{}
+	buf.WriteRune(head)
+
+	for {
+		c, _, err := sc.src.ReadRune()
+		if err == io.EOF {
+			return buf.String(), nil
+		}
+		if err != nil {
+			return "", err
+		}
+		if unicode.IsSpace(c) || c == '(' || c == ')' {
+			sc.src.UnreadRune()
+			return buf.String(), nil
+		}
+
+		buf.WriteRune(c)
+	}
+}
---
+++ scan_test.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"io"
+	"reflect"
+	"strings"
+	"testing"
+)
+
+func TestScanner(t *testing.T) {
+	cases := []struct {
+		src      string
+		expected []string
+	}{
+		{"(a word 101)", []string{"(", "a", "word", "101", ")"}},
+		{"(a (word 101))", []string{"(", "a", "(", "word", "101", ")", ")"}},
+		{`(a (word 101 "moo"))`, []string{"(", "a", "(", "word", "101", `"moo"`, ")", ")"}},
+		{`a<b a-z :name`, []string{"a<b", "a-z", ":name"}},
+		{`"a b c"`, []string{`"a`, `b`, `c"`}}, // とりあえず文字列はなし
+	}
+
+	for _, c := range cases {
+		t.Run("case:"+c.src, func(t *testing.T) {
+			sc := newScanner(strings.NewReader(c.src))
+			toks := []string{}
+			for {
+				tok, err := sc.scan()
+				if err == io.EOF {
+					break
+				}
+				if err != nil {
+					t.Fatal(err)
+				}
+				toks = append(toks, tok)
+			}
+			if !reflect.DeepEqual(c.expected, toks) {
+				t.Errorf("want %q, got %q", c.expected, toks)
+			}
+		})
+	}
+}
